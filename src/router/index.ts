283db27f01e53af24fe41e3a77eba5fad051c26b
Rafly Firmansyah <raflyfirmansyah017@gmail.com>
2024-06-08 08:14:17 +0700

    fix error

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
 import LandingView from "@/views/LandingView.vue";
-
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
